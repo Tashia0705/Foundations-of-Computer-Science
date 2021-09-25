4e36781709761120a02e241273fffb699190ece8
Tashia Boddu <84092012+Tashia0705@users.noreply.github.com>
2021-09-25 13:50:06 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <string>
+#include <functional>
 
 std::vector<int> lexi(std::vector<int> &alph, int N, std::vector<int> &result);
+
+/*
+ Task 4.
+ Representing the set of states (and the set of accepting states) in a class using the
+ constructor
+*/
+template<typename State>
+class DFA {
+  public:
+    DFA(std::function<bool(State)> Qp, State q0p, std::function<State(State, int)> Dp,        std::function<bool (State)> Fp): Q(Qp), q0(q0p), D(Dp), F(Fp) {};
+    std::function<bool(State)> Q;
+    State q0;
+    std::function<State(State, int)> D;
+    std::function<bool (State)> F;
+    
+};
+
+/*
+ Task 7.
+ DFA that accepts only strings of exactly that character
+*/
+DFA<int> task7(int ch) {
+    DFA<int> *z = new DFA<int>(
+        [](int x) { return (x == 0) || (x == 1) || (x == 2); },
+        0,
+        [=](int qi, int c) { if(c == ch && qi == 0) return 1; else return 2; },
+        [](int qi) { return qi == 1; } );
+    return z;
+}
+
 
 int main(int argc, const char * argv[]) {
     /*
@@ -13,31 +45,55 @@ int main(int argc, const char * argv[]) {
     */
     
     std::vector<int> set;
-    set.push_back(0);
-    set.push_back(1);
-    set.push_back(2);
+    for(int i = 0; i <= 9; i++) {
+        set.push_back(i);
+    }
     
     /*
-     Task 2.
+     Task 2 / Task 3 continuation
      Strings are sequences of characters or "abstract elements drawn
      from an alphabet."
      ex. {1,0,1} is a sequence taken from the alphabet {0,1}
     */
     
-    // Task 3 continuation
+    for(int j = 0; j < 30; j++) {
+        std::vector<int> result;
+        lexi(set, j, result);
     
-    std::vector<int> result;
-    lexi(set, 5, result);
-    
-    for(int i = 0; i < result.size(); i++) {
-        std::cout << result[i];
+        for(int i = 0; i < result.size(); i++) {
+            std::cout << result[i];
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
     
+    /*
+     Task 5.
+     DFA that accepts no string - has no accepting state
+    */
+    DFA<int> *x = new DFA<int>(
+        [](int x) { return (x == 0); },
+        0,
+        [](int qi, int c) { return 0; },
+        [](int qi) { return qi == 1; }
+    );
+    std::cout << x->D(0, 1) << std::endl;
+
+    /*
+     Task 6.
+     DFA that accepts only empty string - accepting state is the start state and any other
+     char passed
+    */
+    DFA<int> *y = new DFA<int>(
+        [](int x) { return (x == 1) || (x == 2); },
+        1,
+        [](int qi, int c) { return 2; },
+        [](int qi) { return qi == 1; }
+    );
+    std::cout << y->D(0, 0) << std::endl;
     
     return 0;
 }
-
+    
 /*
  Task 3.
  Function that generates the Nth string of a given alphabet’s lexicographic order
@@ -45,31 +101,35 @@ int main(int argc, const char * argv[]) {
 
 std::vector<int> lexi(std::vector<int> &alph, int N, std::vector<int> &result) {
     if(N == 0) {
-        std::cout << "Empty Set";
+        std::cout << "";
     }
+    else {
     
-    // generate the value of the nth string
-    unsigned long size = alph.size();
-    int i = 0;
-    int num = pow(size,i);
+        // generate the value of the nth string
+        unsigned long size = alph.size();
+        int i = 0;
+        int num = pow(size,i);
     
-    while(N > num) {
-        N -= num;
-        i++;
-        num = pow(size, i);
-    }
+        while(N >= num) {
+            N -= num;
+            i++;
+            num = pow(size, i);
+        }
     
-    // convert decimal to the right base system
-    while(N > 0) {
-        result.insert(result.begin(), N % size);
-        N = N / size;
-    }
-    
-    if(result.capacity() < i) {
-        for(int j = 0; j < i - result.capacity(); j++) {
+        // convert decimal to the right base system
+        while(N > 0) {
+            result.insert(result.begin(), N % size);
+            N = N / size;
+        }
+        // adds 0s in the begining of string if needed
+        if(result.size() < i) {
+            for(int j = 0; j <= i - result.size(); j++) {
                 result.insert(result.begin(), 0);
+            }
         }
     }
 
     return result;
 }
+
+
